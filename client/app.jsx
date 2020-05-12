5ce84d017ba0661e90e386c6548f8f93482c102e
stephenJohnGiardina <stephengia@yahoo.com>
2020-05-12 10:34:50 -0400

    Deleted gitkeep files

--- client/app.jsx
+++ client/app.jsx
@@ -161,4 +161,6 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// ReactDOM.render(<App />, document.getElementById('app'));
+
+export default App;
